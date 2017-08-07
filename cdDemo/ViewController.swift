b82af46d705423542bb15b1f65ff3379e0b131fd
West Kraemer <kraemerwest@gmail.com>
2017-08-07 09:19:31 -0400

    add user and age/password/username to Users

--- cdDemo/ViewController.swift
+++ cdDemo/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("west", forKey: "username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(32, forKey: "age")
+        
+        do {
+            
+            try context.save()
+            
+            print("Saved")
+            
+        }
+        catch {
+            
+            print("There was an error")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
